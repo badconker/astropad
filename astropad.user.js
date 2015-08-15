e5f6bc34a1ea42b3e6edf262db8799729aa7602a
badconker <badconker@users.noreply.github.com>
2015-08-15 08:32:47 +0200

    Version 0.25.6

--- astropad.user.js
+++ astropad.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       AstroPad
-// @version    0.25.5
+// @version    0.25.6
 // @grant      GM_xmlhttpRequest
 // @match      http://mush.vg/*
 // @match      http://mush.vg/#
